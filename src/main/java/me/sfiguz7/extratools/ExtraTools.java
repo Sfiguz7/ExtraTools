3f126f07adb5281a50ba12cc38d00ea66df16400
Sfiguz7 <ale.colli98@gmail.com>
2020-04-01 20:16:38 +0200

    Removed Electric Crucibles; added Cobblestone Generator

--- src/main/java/me/sfiguz7/extratools/ExtraTools.java
+++ src/main/java/me/sfiguz7/extratools/ExtraTools.java
@@ -6,8 +6,8 @@ import io.github.thebusybiscuit.slimefun4.utils.LoreBuilder;
 import me.mrCookieSlime.Slimefun.Lists.SlimefunItems;
 import me.mrCookieSlime.Slimefun.Objects.Research;
 import me.mrCookieSlime.Slimefun.api.Slimefun;
+import me.sfiguz7.extratools.implementation.machines.CobblestoneGenerator;
 import me.sfiguz7.extratools.implementation.machines.ElectricComposter;
-import me.sfiguz7.extratools.implementation.machines.ElectricCrucible;
 import me.sfiguz7.extratools.implementation.machines.GoldTransmuter;
 import me.sfiguz7.extratools.implementation.tools.Hammer;
 import org.bukkit.Material;
@@ -24,10 +24,10 @@ import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 
 public class ExtraTools extends JavaPlugin implements SlimefunAddon {
 
-	private int researchId = 4100;
-	
-	@Override
-	public void onEnable() {
+    private int researchId = 4100;
+
+    @Override
+    public void onEnable() {
 
 		/*
 		Config cfg = new Config(this);
@@ -36,148 +36,179 @@ public class ExtraTools extends JavaPlugin implements SlimefunAddon {
 		}
 		*/
 
-		int bStatsId = 6945;
-		new Metrics(this, bStatsId);
+        int bStatsId = 6945;
+        new Metrics(this, bStatsId);
 
-		Category extra_tools = new Category(new NamespacedKey(this, "extra_tools"), new CustomItem(Material.DIAMOND_AXE, "&4Extra Tools"));
-
-
-
-		SlimefunItemStack HAMMER = new SlimefunItemStack("HAMMER", Material.IRON_PICKAXE, "&cHammer", "", "&9Pulverizes blocks");
-
-		ItemStack[] recipe = {
-				new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT),
-				new ItemStack(Material.IRON_INGOT), new ItemStack(Material.STICK), new ItemStack(Material.IRON_INGOT),
-				null, new ItemStack(Material.STICK), null
-		};
-
-		new Hammer(extra_tools, HAMMER, RecipeType.MAGIC_WORKBENCH, recipe).register(this);
-
-		researchId++;
-		Slimefun.registerResearch(new Research(new NamespacedKey(this, "hammer"), researchId, "Hammer", 3), HAMMER);
+        Category extra_tools = new Category(new NamespacedKey(this,
+                "extra_tools"),
+                new CustomItem(Material.DIAMOND_AXE, "&4Extra Tools")
+        );
 
 
+        SlimefunItemStack hammer = new SlimefunItemStack("HAMMER",
+                Material.IRON_PICKAXE,
+                "&cHammer",
+                "",
+                "&9Pulverizes blocks"
+        );
 
-		SlimefunItemStack GOLD_TRANSMUTER = new SlimefunItemStack("GOLD_TRANSMUTER", Material.YELLOW_TERRACOTTA, "&6Gold Transmuter", "", LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE), LoreBuilder.powerBuffer(256), LoreBuilder.powerPerSecond(18));
+        new Hammer(extra_tools, hammer, RecipeType.MAGIC_WORKBENCH,
+                new ItemStack[]{new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT),
+                        new ItemStack(Material.IRON_INGOT), new ItemStack(Material.STICK), new ItemStack(Material.IRON_INGOT),
+                        null, new ItemStack(Material.STICK), null}
+        ).register(this);
 
-		new GoldTransmuter(extra_tools, GOLD_TRANSMUTER, RecipeType.ENHANCED_CRAFTING_TABLE,
-				new ItemStack[] {null, SlimefunItems.SILVER_INGOT, null, SlimefunItems.ELECTRIC_MOTOR, SlimefunItems.GOLD_24K_BLOCK, SlimefunItems.ELECTRIC_MOTOR, new ItemStack(Material.GOLDEN_PICKAXE), SlimefunItems.MEDIUM_CAPACITOR, new ItemStack(Material.GOLDEN_PICKAXE)}) {
-
-			@Override
-			public int getEnergyConsumption() {
-				return 9;
-			}
-
-			@Override
-			public int getSpeed() {
-				return 1;
-			}
-
-		}.register(this);
-
-		researchId++;
-		Slimefun.registerResearch(new Research(new NamespacedKey(this, "gold_transmuter"), researchId, "Gold_Transmuter", 12), GOLD_TRANSMUTER);
-
+        Slimefun.registerResearch(new Research(new NamespacedKey(this, "hammer"),
+                        ++researchId,
+                        "Hammer",
+                        3),
+                hammer
+        );
 
 
-		SlimefunItemStack ELECTRIC_COMPOSTER = new SlimefunItemStack("ELECTRIC_COMPOSTER", Material.MAGENTA_TERRACOTTA, "&cElectric Composter", "", LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE), "&8\u21E8 &7Speed: 1x", LoreBuilder.powerBuffer(256), LoreBuilder.powerPerSecond(18));
-		SlimefunItemStack ELECTRIC_COMPOSTER_2 = new SlimefunItemStack("ELECTRIC_COMPOSTER_2", Material.MAGENTA_TERRACOTTA, "&cElectric Composter &7(&eII&7)", "", LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE), "&8\u21E8 &7Speed: 4x", LoreBuilder.powerBuffer(256), LoreBuilder.powerPerSecond(50));
+        SlimefunItemStack gold_transmuter = new SlimefunItemStack("GOLD_TRANSMUTER",
+                Material.YELLOW_TERRACOTTA,
+                "&6Gold Transmuter",
+                "",
+                LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE),
+                LoreBuilder.powerBuffer(256),
+                LoreBuilder.powerPerSecond(18)
+        );
+
+        new GoldTransmuter(extra_tools, gold_transmuter, RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{null, SlimefunItems.SILVER_INGOT, null,
+                        SlimefunItems.ELECTRIC_MOTOR, SlimefunItems.GOLD_24K_BLOCK, SlimefunItems.ELECTRIC_MOTOR,
+                        new ItemStack(Material.GOLDEN_PICKAXE), SlimefunItems.MEDIUM_CAPACITOR, new ItemStack(Material.GOLDEN_PICKAXE)}
+        ) {
+
+            @Override
+            public int getEnergyConsumption() {
+                return 9;
+            }
+
+            @Override
+            public int getSpeed() {
+                return 1;
+            }
+
+        }.register(this);
+
+        Slimefun.registerResearch(new Research(new NamespacedKey(this, "gold_transmuter"),
+                        ++researchId,
+                        "Gold_Transmuter",
+                        12),
+                gold_transmuter
+        );
 
 
-		new ElectricComposter(extra_tools, ELECTRIC_COMPOSTER, RecipeType.ENHANCED_CRAFTING_TABLE,
-				new ItemStack[] {SlimefunItems.GILDED_IRON, SlimefunItems.MAGNESIUM_INGOT, SlimefunItems.GILDED_IRON, SlimefunItems.ELECTRIC_MOTOR, SlimefunItems.COMPOSTER, SlimefunItems.ELECTRIC_MOTOR, new ItemStack(Material.IRON_HOE), SlimefunItems.MEDIUM_CAPACITOR, new ItemStack(Material.IRON_HOE)}) {
+        SlimefunItemStack electric_composter = new SlimefunItemStack("ELECTRIC_COMPOSTER",
+                Material.MAGENTA_TERRACOTTA,
+                "&cElectric Composter",
+                "",
+                LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE),
+                "&8\u21E8 &7Speed: 1x",
+                LoreBuilder.powerBuffer(256),
+                LoreBuilder.powerPerSecond(18)
+        );
+        SlimefunItemStack electric_composter_2 = new SlimefunItemStack("ELECTRIC_COMPOSTER_2",
+                Material.MAGENTA_TERRACOTTA,
+                "&cElectric Composter &7(&eII&7)",
+                "",
+                LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE),
+                "&8\u21E8 &7Speed: 4x",
+                LoreBuilder.powerBuffer(256),
+                LoreBuilder.powerPerSecond(50)
+        );
 
-			@Override
-			public int getEnergyConsumption() {
-				return 9;
-			}
+        new ElectricComposter(extra_tools, electric_composter, RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{SlimefunItems.GILDED_IRON, SlimefunItems.MAGNESIUM_INGOT, SlimefunItems.GILDED_IRON,
+                        SlimefunItems.ELECTRIC_MOTOR, SlimefunItems.COMPOSTER, SlimefunItems.ELECTRIC_MOTOR,
+                        new ItemStack(Material.IRON_HOE), SlimefunItems.MEDIUM_CAPACITOR, new ItemStack(Material.IRON_HOE)}
+        ) {
 
-			@Override
-			public int getSpeed() {
-				return 1;
-			}
+            @Override
+            public int getEnergyConsumption() {
+                return 9;
+            }
 
-		}.register(this);
-		new ElectricComposter(extra_tools, ELECTRIC_COMPOSTER_2, RecipeType.ENHANCED_CRAFTING_TABLE,
-				new ItemStack[] {SlimefunItems.HARDENED_METAL_INGOT, SlimefunItems.BLISTERING_INGOT_3, SlimefunItems.HARDENED_METAL_INGOT, SlimefunItems.ELECTRIC_MOTOR, ELECTRIC_COMPOSTER, SlimefunItems.ELECTRIC_MOTOR, new ItemStack(Material.DIAMOND_HOE), SlimefunItems.LARGE_CAPACITOR, new ItemStack(Material.DIAMOND_HOE)}) {
+            @Override
+            public int getSpeed() {
+                return 1;
+            }
 
-			@Override
-			public int getEnergyConsumption() {
-				return 25;
-			}
+        }.register(this);
+        new ElectricComposter(extra_tools, electric_composter_2, RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{SlimefunItems.HARDENED_METAL_INGOT, SlimefunItems.BLISTERING_INGOT_3, SlimefunItems.HARDENED_METAL_INGOT,
+                        SlimefunItems.ELECTRIC_MOTOR, electric_composter, SlimefunItems.ELECTRIC_MOTOR,
+                        new ItemStack(Material.DIAMOND_HOE), SlimefunItems.LARGE_CAPACITOR, new ItemStack(Material.DIAMOND_HOE)}
+        ) {
 
-			@Override
-			public int getSpeed() {
-				return 4;
-			}
+            @Override
+            public int getEnergyConsumption() {
+                return 25;
+            }
 
-		}.register(this);
+            @Override
+            public int getSpeed() {
+                return 4;
+            }
 
-		researchId++;
-		Slimefun.registerResearch(new Research(new NamespacedKey(this, "electric_composter"), researchId, "Electric_Composter", 18), ELECTRIC_COMPOSTER);
-		researchId++;
-		Slimefun.registerResearch(new Research(new NamespacedKey(this, "electric_composter_2"), researchId, "Electric_Composter_2", 18), ELECTRIC_COMPOSTER_2);
+        }.register(this);
 
-
-
-		SlimefunItemStack ELECTRIC_CRUCIBLE = new SlimefunItemStack("ELECTRIC_CRUCIBLE", Material.BLACK_TERRACOTTA, "&cElectric Crucible", "", LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE), "&8\u21E8 &7Speed: 1x", LoreBuilder.powerBuffer(256), LoreBuilder.powerPerSecond(18));
-		SlimefunItemStack ELECTRIC_CRUCIBLE_2 = new SlimefunItemStack("ELECTRIC_CRUCIBLE_2", Material.BLACK_TERRACOTTA, "&cElectric Crucible &7(&eII&7)", "", LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE), "&8\u21E8 &7Speed: 4x", LoreBuilder.powerBuffer(256), LoreBuilder.powerPerSecond(50));
-
-
-		new ElectricCrucible(extra_tools, ELECTRIC_CRUCIBLE, RecipeType.ENHANCED_CRAFTING_TABLE,
-				new ItemStack[] {new ItemStack(Material.LAVA_BUCKET), SlimefunItems.CORINTHIAN_BRONZE_INGOT, new ItemStack(Material.LAVA_BUCKET), SlimefunItems.ELECTRIC_MOTOR, SlimefunItems.CRUCIBLE, SlimefunItems.ELECTRIC_MOTOR, new ItemStack(Material.IRON_PICKAXE), SlimefunItems.MEDIUM_CAPACITOR, new ItemStack(Material.IRON_PICKAXE)}) {
-
-			@Override
-			public int getEnergyConsumption() {
-				return 9;
-			}
-
-			@Override
-			public int getSpeed() {
-				return 1;
-			}
-
-		}.register(this);
-		new ElectricCrucible(extra_tools, ELECTRIC_CRUCIBLE_2, RecipeType.ENHANCED_CRAFTING_TABLE,
-				new ItemStack[] {SlimefunItems.REINFORCED_ALLOY_INGOT, SlimefunItems.BLISTERING_INGOT_3, SlimefunItems.REINFORCED_ALLOY_INGOT, SlimefunItems.ELECTRIC_MOTOR, ELECTRIC_CRUCIBLE, SlimefunItems.ELECTRIC_MOTOR, new ItemStack(Material.DIAMOND_PICKAXE), SlimefunItems.LARGE_CAPACITOR, new ItemStack(Material.DIAMOND_PICKAXE)}) {
-
-			@Override
-			public int getEnergyConsumption() {
-				return 25;
-			}
-
-			@Override
-			public int getSpeed() {
-				return 4;
-			}
-
-		}.register(this);
-
-		researchId++;
-		Slimefun.registerResearch(new Research(new NamespacedKey(this, "electric_crucible"), researchId, "Electric_Crucible", 16), ELECTRIC_CRUCIBLE);
-		researchId++;
-		Slimefun.registerResearch(new Research(new NamespacedKey(this, "electric_crucible_2"), researchId, "Electric_Crucible_2", 28), ELECTRIC_CRUCIBLE_2);
+        Slimefun.registerResearch(new Research(new NamespacedKey(this, "electric_composter"),
+                        ++researchId,
+                        "Electric_Composter",
+                        18),
+                electric_composter
+        );
+        Slimefun.registerResearch(new Research(new NamespacedKey(this, "electric_composter_2"),
+                        ++researchId,
+                        "Electric_Composter_2",
+                        18),
+                electric_composter_2
+        );
 
 
 
+        SlimefunItemStack cobblestoneGenerator = new SlimefunItemStack("COBBLESTONE_GENERATOR",
+                Material.POLISHED_ANDESITE,
+                "&cCobblestone Generator",
+                "",
+                LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE),
+                LoreBuilder.powerBuffer(256),
+                LoreBuilder.powerPerSecond(36)
+        );
+
+        new CobblestoneGenerator(extra_tools, cobblestoneGenerator, RecipeType.ENHANCED_CRAFTING_TABLE,
+                new ItemStack[]{SlimefunItems.PROGRAMMABLE_ANDROID_MINER, SlimefunItems.MAGNESIUM_INGOT, SlimefunItems.PROGRAMMABLE_ANDROID_MINER,
+                        new ItemStack(Material.WATER_BUCKET), SlimefunItems.BLISTERING_INGOT_3, new ItemStack(Material.LAVA_BUCKET),
+                        SlimefunItems.PROGRAMMABLE_ANDROID_MINER, SlimefunItems.BIG_CAPACITOR, SlimefunItems.PROGRAMMABLE_ANDROID_MINER,}
+        ).register(this);
+
+        Slimefun.registerResearch(new Research(new NamespacedKey(this, "cobblestone_generator"),
+                        ++researchId,
+                        "Cobblestone_Generator",
+                        40),
+                cobblestoneGenerator
+        );
 
 
-	}
-	
-	@Override
-	public void onDisable() {
-		// Logic for disabling the plugin...
-	}
+    }
 
-	@Override
-	public String getBugTrackerURL() {
-		// You can return a link to your Bug Tracker instead of null here
-		return null;
-	}
+    @Override
+    public void onDisable() {
+        // Logic for disabling the plugin...
+    }
 
-	@Override
-	public JavaPlugin getJavaPlugin() {
-		return this;
-	}
+    @Override
+    public String getBugTrackerURL() {
+        // You can return a link to your Bug Tracker instead of null here
+        return null;
+    }
+
+    @Override
+    public JavaPlugin getJavaPlugin() {
+        return this;
+    }
 
 }
---
+++ src/main/java/me/sfiguz7/extratools/implementation/machines/CobblestoneGenerator.java
@@ -0,0 +1,126 @@
+package me.sfiguz7.extratools.implementation.machines;
+
+import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
+import io.github.thebusybiscuit.slimefun4.core.networks.energy.EnergyNetComponentType;
+import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
+import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
+import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
+import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu;
+import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ClickAction;
+import me.mrCookieSlime.Slimefun.Lists.RecipeType;
+import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SimpleSlimefunItem;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.interfaces.InventoryBlock;
+import me.mrCookieSlime.Slimefun.Objects.handlers.BlockTicker;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import me.mrCookieSlime.Slimefun.api.energy.ChargableBlock;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
+import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+
+public class CobblestoneGenerator extends SimpleSlimefunItem<BlockTicker> implements InventoryBlock, EnergyNetComponent {
+
+    private static final int ENERGY_CONSUMPTION = 32;
+    private int decrement = 20;
+
+    private final int[] border = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 18, 19, 20, 21, 22, 27, 28, 29, 30, 31, 36, 37, 38, 39, 40, 41, 42, 43, 44, 22};
+    private final int[] inputBorder = {};
+    private final int[] outputBorder = {14, 15, 16, 17, 23, 26, 32, 33, 34, 35};
+
+    public CobblestoneGenerator(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(category, item, recipeType, recipe);
+
+        createPreset(this, this::constructMenu);
+    }
+
+    private void constructMenu(BlockMenuPreset preset) {
+        for (int i : border) {
+            preset.addItem(i, new CustomItem(new ItemStack(Material.GRAY_STAINED_GLASS_PANE), " "), ChestMenuUtils.getEmptyClickHandler());
+        }
+
+        for (int i : inputBorder) {
+            preset.addItem(i, new CustomItem(new ItemStack(Material.CYAN_STAINED_GLASS_PANE), " "), ChestMenuUtils.getEmptyClickHandler());
+        }
+        for (int i : outputBorder) {
+            preset.addItem(i, new CustomItem(new ItemStack(Material.ORANGE_STAINED_GLASS_PANE), " "), ChestMenuUtils.getEmptyClickHandler());
+        }
+
+        for (int i : getOutputSlots()) {
+            preset.addMenuClickHandler(i, new ChestMenu.AdvancedMenuClickHandler() {
+
+                @Override
+                public boolean onClick(Player p, int slot, ItemStack cursor, ClickAction action) {
+                    return false;
+                }
+
+                @Override
+                public boolean onClick(InventoryClickEvent e, Player p, int slot, ItemStack cursor, ClickAction action) {
+                    return cursor == null || cursor.getType() == Material.AIR;
+                }
+            });
+        }
+    }
+
+    @Override
+    public int[] getInputSlots() {
+        return new int[]{};
+    }
+
+    @Override
+    public int[] getOutputSlots() {
+        return new int[]{24, 25};
+    }
+
+    @Override
+    public EnergyNetComponentType getEnergyComponentType() {
+        return EnergyNetComponentType.CONSUMER;
+    }
+
+    @Override
+    public int getCapacity() {
+        return 512;
+    }
+
+    @Override
+    public BlockTicker getItemHandler() {
+        return new BlockTicker() {
+
+            @Override
+            public void tick(Block b, SlimefunItem sf, Config data) {
+
+                if (--decrement != 0) {
+                    return;
+                }
+
+                decrement = 2;
+                ItemStack output = new ItemStack(Material.COBBLESTONE);
+
+                if (ChargableBlock.getCharge(b) >= ENERGY_CONSUMPTION) {
+                    BlockMenu menu = BlockStorage.getInventory(b);
+
+                    if (!menu.fits(output, getOutputSlots())) {
+                        return;
+                    }
+
+                    ChargableBlock.addCharge(b, -ENERGY_CONSUMPTION);
+                    menu.pushItem(output, getOutputSlots());
+                }
+            }
+
+            @Override
+            public boolean isSynchronized() {
+                return true;
+            }
+        };
+    }
+
+}
+
