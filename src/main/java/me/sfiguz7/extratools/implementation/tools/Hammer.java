275dc1205de92f2f96e650fe6f9e603677d5c156
Sfiguz7 <ale.colli98@gmail.com>
2020-10-28 22:34:50 +0100

    Fixed(?) Hammer

--- src/main/java/me/sfiguz7/extratools/implementation/tools/Hammer.java
+++ src/main/java/me/sfiguz7/extratools/implementation/tools/Hammer.java
@@ -1,6 +1,6 @@
 package me.sfiguz7.extratools.implementation.tools;
 
-import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
+import io.github.thebusybiscuit.slimefun4.core.handlers.ToolUseHandler;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import io.github.thebusybiscuit.slimefun4.implementation.items.SimpleSlimefunItem;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
@@ -8,89 +8,66 @@ import me.mrCookieSlime.Slimefun.api.Slimefun;
 import me.sfiguz7.extratools.lists.ETItems;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.List;
-
-public class Hammer extends SimpleSlimefunItem<BlockBreakHandler> {
+public class Hammer extends SimpleSlimefunItem<ToolUseHandler> {
 
     public Hammer() {
         super(ETItems.extra_tools, ETItems.HAMMER, RecipeType.MAGIC_WORKBENCH,
-                new ItemStack[]{new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT),
-                        new ItemStack(Material.IRON_INGOT),
-                        new ItemStack(Material.IRON_INGOT), new ItemStack(Material.STICK), new ItemStack(Material.IRON_INGOT),
-                        null, new ItemStack(Material.STICK), null});
+            new ItemStack[] {new ItemStack(Material.IRON_INGOT), new ItemStack(Material.IRON_INGOT),
+                new ItemStack(Material.IRON_INGOT),
+                new ItemStack(Material.IRON_INGOT), new ItemStack(Material.STICK), new ItemStack(Material.IRON_INGOT),
+                null, new ItemStack(Material.STICK), null});
     }
 
     @Override
-    public BlockBreakHandler getItemHandler() {
-        return new BlockBreakHandler() {
+    public ToolUseHandler getItemHandler() {
+        return (e, tool, fortune, drops) -> {
+            if (Slimefun.hasUnlocked(e.getPlayer(), Hammer.this, true)) {
 
-            @Override
-            public boolean isPrivate() {
-                return false;
-            }
+                Block b = e.getBlock();
+                ItemStack drop = getDrop(b);
 
-            @Override
-            public boolean onBlockBreak(BlockBreakEvent e, ItemStack item, int fortune, List<ItemStack> drops) {
-                if (isItem(item)) {
-                    if (!Slimefun.hasUnlocked(e.getPlayer(), Hammer.this, true)) {
-                        return true;
-                    }
-
-                    Block b = e.getBlock();
-                    ItemStack drop = this.getDrop(b);
-
-                    if (drop == null) {
-                        return true;
-                    }
-
+                if (drop != null) {
                     //Can't throw NPEs now
                     b.getLocation().getWorld().dropItemNaturally(b.getLocation(), drop);
                     e.setDropItems(false);
-                    return true;
                 }
-                return false;
             }
-
-
-            public ItemStack getDrop(Block b) {
-
-                Material m = b.getType();
-                System.out.println();
-
-                switch (m) {
-                    case STONE:
-                    case GRANITE:
-                    case DIORITE:
-                    case ANDESITE:
-                    case COBBLESTONE: {
-                        return new ItemStack(Material.GRAVEL);
-                    }
-                    case GRAVEL:
-                    case GRASS_BLOCK:
-                    case DIRT:
-                    case COARSE_DIRT:
-                    case PODZOL: {
-                        return new ItemStack(Material.SAND);
-                    }
-                    case IRON_ORE: {
-                        return SlimefunItems.IRON_DUST;
-                    }
-                    case GOLD_ORE: {
-                        return SlimefunItems.GOLD_DUST;
-                    }
-                    case NETHERRACK: {
-                        return new ItemStack(Material.SOUL_SAND);
-                    }
-                }
-
-                return null;
-            }
-
-
         };
     }
 
+
+    public ItemStack getDrop(Block b) {
+
+        Material m = b.getType();
+
+        switch (m) {
+            case STONE:
+            case GRANITE:
+            case DIORITE:
+            case ANDESITE:
+            case COBBLESTONE: {
+                return new ItemStack(Material.GRAVEL);
+            }
+            case GRAVEL:
+            case GRASS_BLOCK:
+            case DIRT:
+            case COARSE_DIRT:
+            case PODZOL: {
+                return new ItemStack(Material.SAND);
+            }
+            case IRON_ORE: {
+                return SlimefunItems.IRON_DUST;
+            }
+            case GOLD_ORE: {
+                return SlimefunItems.GOLD_DUST;
+            }
+            case NETHERRACK: {
+                return new ItemStack(Material.SOUL_SAND);
+            }
+        }
+
+        return null;
+    }
 }
